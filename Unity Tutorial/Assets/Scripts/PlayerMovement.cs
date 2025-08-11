0a5493b95c8cfe4e33bf5d462397595ce852759a
BHASVIC-WilliamWinter24 <william.winter24@students.bhasvic.ac.uk>
2025-08-11 11:53:57 +0100

    Completed 'Player Movement'

--- Unity Tutorial/Assets/Scripts/PlayerMovement.cs
+++ Unity Tutorial/Assets/Scripts/PlayerMovement.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public float speed = 5f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +16,15 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // get the axes of input
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        // the 2D vector is equal to the current position
+        Vector2 pos = transform.position;
+
+        pos.x += h * speed * Time.deltaTime;
+        pos.y += v * speed * Time.deltaTime;
+        // new position is equal to the vector
+        transform.position = pos;
     }
 }
